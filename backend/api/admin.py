464621c81a6b3b9d0963d678acd68c1c0af2b445
artem.puhach <artem.puhach@cyberpolice.gov.ua>
2026-03-14 13:29:02 +0200

    register models in admin

---
+++ backend/api/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+
+from .models import Generation, GenerationJob
+
+
+@admin.register(Generation)
+class GenerationAdmin(admin.ModelAdmin):
+    list_display = ("id", "created_at", "endpoint", "source_image_sha256")
+    list_filter = ("endpoint", "created_at")
+    search_fields = ("id", "source_image_sha256", "source_image_url")
+    readonly_fields = ("id", "created_at", "source_image_sha256", "source_image_url", "endpoint")
+
+
+@admin.register(GenerationJob)
+class GenerationJobAdmin(admin.ModelAdmin):
+    list_display = ("id", "created_at", "updated_at", "style_key", "status", "fal_request_id")
+    list_filter = ("status", "style_key", "created_at")
+    search_fields = ("id", "fal_request_id", "style_key", "style_label")
+    readonly_fields = (
+        "id",
+        "created_at",
+        "updated_at",
+        "fal_request_id",
+        "style_key",
+        "style_label",
+        "prompt",
+        "status",
+        "result_images",
+        "error",
+        "generation",
+    )
+
